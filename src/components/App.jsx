30961f95b193df1f8c4f912785e4bf0ddfe3bcd9
annared <annared1982@gmail.com>
2023-02-22 15:17:11 +0200

    compon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { ContactForm } from 'components/ContactForm/ContactForm';
 
 export class App extends Component {
   state = {
     contacts: [],
-    name: '',
-    number: '',
   };
 
   addContact = (name, number) => {
@@ -22,52 +21,18 @@ export class App extends Component {
     }));
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const { name, number } = e.target.elements;
-    // console.log(this.state);
-    this.addContact(name.value, number.value);
-    this.setState({ name: '', number: '' });
-  };
-
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-
-          <label htmlFor="number">
-            Number
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-
-          <button type="submit">Add contact</button>
-        </form>
+        <ContactForm onSubmit={this.addContact} />
 
         <h2>Contacts</h2>
-
         <ul>
           {this.state.contacts.map(contact => {
             return (
               <li key={contact.id}>
-                {contact.name}:{contact.number}
+                {contact.name}: {contact.number}
               </li>
             );
           })}
