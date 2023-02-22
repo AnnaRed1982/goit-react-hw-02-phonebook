2de6d683c4f62bb4007d368498d74e45a2142944
annared <annared1982@gmail.com>
2023-02-22 15:47:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,12 @@ import { ContactList } from 'components/ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
   };
 
   addContact = (name, number) => {
@@ -27,6 +32,10 @@ export class App extends Component {
         <ContactForm onSubmit={this.addContact} />
 
         <h2>Contacts</h2>
+
+        <h3>Find contact by name</h3>
+        <input type="text" name="filter" />
+        
         <ContactList contacts={this.state.contacts} />
       </div>
     );
